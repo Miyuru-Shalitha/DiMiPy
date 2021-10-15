8a4fa59744c95ee4995f00f5701f00fd292be753
Miyuru <miyurushalitha@gmail.com>
2021-10-15 19:38:18 +0530

    Edit with of login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -90,10 +90,15 @@ const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  width: 30%;
+  width: 50%;
   height: 30%;
   padding: 3rem;
   border: 1px solid #999;
   border-radius: 0.2rem;
   box-shadow: 1px 1px 15px 1px #999;
+
+  @media only screen and (max-width: 37.5em) {
+    /* 600px */
+    width: 60%;
+  }
 `;
